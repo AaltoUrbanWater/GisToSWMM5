e6f4c25dfe225760559cc0b8f1c4fd62045a91af
Tero Niemi <tero.niemi@aalto.fi>
2018-04-18 13:41:24 +0300

    Upload New File

---
+++ run_scripts/run_swmmtoolbox.bat
@@ -0,0 +1,26 @@
+@echo off
+REM Extract data from SWMM binaries and save to text file
+
+REM Activate conda environment (with necessary packages installed)
+call activate swmmtoolbox
+
+REM Run swmmtoolbox
+REM List the objects in output file
+echo List objects in output file:
+swmmtoolbox catalog "C:\LocalUserData\User-data\tjniemi\GisToSWMM5\demo_catchment\out\SWMM_out\demo_catchment_adap.out"
+echo **********
+
+REM List variables available for each type (subcatchment, node, link, pollutant, system)
+echo List variables and types in output file:
+swmmtoolbox listvariables "C:\LocalUserData\User-data\tjniemi\GisToSWMM5\demo_catchment\out\SWMM_out\demo_catchment_adap.out"
+echo **********
+
+REM Extract system runoff and save to new file
+echo Extracting system runoff and saving to new file.
+swmmtoolbox extract "C:\LocalUserData\User-data\tjniemi\GisToSWMM5\demo_catchment\out\SWMM_out\demo_catchment_adap.out" system,Runoff,4 > system_runoff.txt
+
+REM Deactivate conda environment
+call deactivate
+
+REM Wait for user input before closing terminal
+pause 
