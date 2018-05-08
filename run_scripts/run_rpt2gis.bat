a6e9f490b50cca8dc0d748b6d93d995977a9cc0a
Tero Niemi <tero.niemi@aalto.fi>
2018-05-08 16:33:47 +0300

    Replace run_rpt2gis.bat

--- run_scripts/run_rpt2gis.bat
+++ run_scripts/run_rpt2gis.bat
@@ -1,12 +1,19 @@
 @echo off
-REM Extract subcatchment simulation results from SWMM report (by default *.rpt) file and save into shapefile with subcatchment geometries from SWMM input (*.inp) file.
+REM Extract subcatchment simulation results from SWMM report (by default *.rpt) file 
+REM and save into shapefile with subcatchment geometries from SWMM input (*.inp) file.
+REM Syntax:
+REM 	python rpt2gis.py [PATH TO *.inp FILE] [PATH TO *.rpt FILE] 'EPSG:[XXXX]'
+
 
 REM Activate conda environment (with necessary packages installed)
 call activate
 
-REM Run rpt2gis
-REM Change the paths and the coordinate reference system code (EPSG:XXXX) as necessary
-python "../utils/rpt2gis.py" "../demo_catchment/out/SWMM_in/demo_catchment_adap.inp" "../demo_catchment/out/SWMM_out/demo_catchment_adap.rpt" "EPSG:3879"
+REM Example of running rpt2gis.py
+REM '^' is used only to split long lines here
+python "../utils/rpt2gis.py" ^
+"../demo_catchment/out/SWMM_in/demo_catchment_rect.inp" ^
+"../demo_catchment/out/SWMM_out/demo_catchment_rect.rpt" ^
+"EPSG:3879"
 
 REM Deactivate conda environment
 call deactivate
