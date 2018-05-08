24e6c3962b2aff3e87ac77219a4b12769ef25415
Tero Niemi <tero.niemi@aalto.fi>
2018-05-08 16:33:27 +0300

    Replace run_inp2gis.bat

--- run_scripts/run_inp2gis.bat
+++ run_scripts/run_inp2gis.bat
@@ -1,12 +1,18 @@
 @echo off
-REM Extract subcatchment geometries and attributes from SWMM input (*.inp) file and save into shapefile.
+REM Extract subcatchment geometries and attributes from SWMM input (*.inp) file 
+REM and save into shapefile.
+REM Syntax:
+REM 	python inp2gis.py [PATH TO *.inp FILE] 'EPSG:[XXXX]'
+
 
 REM Activate conda environment (with necessary packages installed)
 call activate
 
-REM Run inp2gis
-REM Change the paths and the coordinate reference system code (EPSG:XXXX) as necessary
-python "../utils/inp2gis.py" "../demo_catchment/out/SWMM_in/demo_catchment_adap.inp" "EPSG:3879"
+REM Example of running inp2gis.py
+REM '^' is used only to split long lines here
+python "../utils/inp2gis.py" ^
+"../demo_catchment/out/SWMM_in/demo_catchment_rect.inp" ^
+"EPSG:3879"
 
 REM Deactivate conda environment
 call deactivate
