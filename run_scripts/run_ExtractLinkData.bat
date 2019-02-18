0fee13586990327f0e2139a3a26a6f8a6b85ebbf
Tero Niemi <tero.niemi@aalto.fi>
2019-02-18 12:31:57 +0200

    Replace run_ExtractLinkData.bat

--- run_scripts/run_ExtractLinkData.bat
+++ run_scripts/run_ExtractLinkData.bat
@@ -6,7 +6,7 @@ REM Allowed parameters are: 'flow', 'velocity', 'depth', or 'capacity'"
 
 REM Activate conda environment (with necessary packages installed)
 REM (here called 'gistoswmm5')
-call activate gistoswmm5
+call conda activate gistoswmm5
 
 REM Example of running ExtractLinkData.py
 REM '^' is used only to split long lines here
@@ -16,7 +16,7 @@ python "..\utils\ExtractLinkData.py" "<<< Link c96 >>>" "flow"^
  "..\demo_catchment\out\SWMM_results\flow_timeseries.csv"
 
 REM Deactivate conda environment
-call deactivate
+call conda deactivate
 
 REM Wait for user input before closing terminal
 pause
