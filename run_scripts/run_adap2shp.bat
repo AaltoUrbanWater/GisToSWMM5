11dcb204fb4f544685e43eca02bbf50620c23fc6
Tero Niemi <tero.niemi@aalto.fi>
2019-02-18 12:34:06 +0200

    Replace run_adap2shp.bat

--- run_scripts/run_adap2shp.bat
+++ run_scripts/run_adap2shp.bat
@@ -1,12 +1,12 @@
 @echo off
-REM This is an example script to run adap2shp.py in order to produce a shapefile
+REM This is an example script to run adap2shp.py in order to produce a shapefile of
 REM SWMM subcatchments.
 REM Syntax:
 REM 	python adap2shp.py [INFILE_subcatchments.asc] [INFILE.wkt] [OUTFILE.shp] 'epsg:XXXX'
 
 REM Activate conda environment (with necessary packages installed)
 REM (here called 'gistoswmm5')
-call activate gistoswmm5
+call conda activate gistoswmm5
 
 REM Example of running adap2shp.py
 REM '^' is used only to split long lines here
@@ -17,7 +17,7 @@ python "../utils/adap2shp.py"^
  "epsg:3879"
 
 REM Deactivate conda environment
-call deactivate
+call conda deactivate
 
 REM Wait for user input before closing terminal
 pause
